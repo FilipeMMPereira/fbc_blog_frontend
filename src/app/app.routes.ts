907c6bae4a7f24258150101f79af07c790235b23
Filipe Pereira <filipepereira977@gmail.com>
2025-09-10 18:47:07 +0200

    feat: first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Layout } from './layouts/layout/layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+  path: '',
+  component: Layout,
+  children: [
+    {path: '', loadComponent: () => import('./pages/home/home').then(m => m.Home)},
+  ]
+}];
